6918f0b5ef75426bb28b2adcd4e1682f756a73c0
Sophia Ladwiniec <slladwiniec@dons.usfca.edu>
2021-04-05 22:36:27 -0700

    adding assignment

--- sketch.js
+++ sketch.js
@@ -1,136 +1,199 @@
 /***********************************************************************************
-  AllFeatures
+  MoodyMaze
   by Scott Kildall
 
-  Uses the p5.debugScreen.js library to allocate and output simple mouse-clicked
-  messages on the screen. Shows all accessors
-
+  Uses the p5.2DAdventure.js class 
+  
 ------------------------------------------------------------------------------------
 	To use:
 	Add this line to the index.html
 
-  <script src="p5.debugScreen.js"></script>
+  <script src="p5.2DAdventure.js"></script>
 ***********************************************************************************/
 
-// global var for the debug screen object
-var debugScreen;
-var lineCount;      // # of lines for the screen
+// adventure manager global  
+var adventureManager;
 
-// drawing vars
-var yTextPos = 30;
-var lineHeight = 18;
+// p5.play
+var playerSprite;
+var playerAnimation;
 
+// Clickables: the manager class
+var clickablesManager;    // the manager class
+var clickables;           // an array of clickable objects
 
-// Setup code goes here
+// indexes into the clickable array (constants)
+const playGameIndex = 0;
+
+// Allocate Adventure Manager with states table and interaction tables
+function preload() {
+  clickablesManager = new ClickableManager('data/clickableLayout.csv');
+  adventureManager = new AdventureManager('data/adventureStates.csv', 'data/interactionTable.csv', 'data/clickableLayout.csv');
+  chase_image = loadImage('assets/avatars/chase 1.png'); 
+  chase_image_2 = loadImage('assets/avatars/chase 2.png'); 
+}
+
+// Setup the adventure manager
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1280, 720);
 
-  textAlign(CENTER);
-  textSize(lineHeight);
+  // setup the clickables = this will allocate the array
+  clickables = clickablesManager.setup();
 
-  debugScreen = new DebugScreen();
-  lineCount = debugScreen.getNumLines();
- }
+  // create a sprite and add the 3 animations
+  playerSprite = createSprite(158, 122, 80, 80);
 
-
-// Draw code goes here
-function draw() {
-  background(128);
-
-  // draw text instuctions
-  fill(255);
-  textAlign(LEFT)
-  let textX = width/2;
-  text( "Click on mouse to add debug text", textX, yTextPos);  
-  text( "Press [t] to switch to top-screen debug screen", textX, yTextPos + lineHeight); 
-  text( "Press [b] to switch to bottom-screen debug screen", textX, yTextPos + lineHeight*2); 
-  text( "Press [a] to turn ON autoscroll", textX, yTextPos + lineHeight*3); 
-  text( "Press [m] to turn OFF autoscroll", textX, yTextPos + lineHeight*4); 
-  text( "Press [x] to turn OFF background rect", textX, yTextPos + lineHeight*5); 
-  text( "Press [y] to turn ON background rect", textX, yTextPos + lineHeight*6); 
-  text( "Press [r] to change color to RED", textX, yTextPos + lineHeight*7); 
-  text( "Press [g] to change color to GREEN", textX, yTextPos + lineHeight*8); 
-  text( "Press UP ARROW to increase font size", textX, yTextPos + lineHeight*9);
-  text( "Press DOWN ARROW to decrease font size", textX, yTextPos + lineHeight*10);
-
-  // draw debug srreen
-  debugScreen.draw();
-}
-
-function keyPressed() {
-  // draw from top
-  if( key === 't' ) {
-    debugScreen.drawFromTop();
-    debugScreen.print("setting to draw from top");
-    return;
-  }
-
-  // draw from bottom
-  if( key === 'b' ) {
-    debugScreen.drawFromBottom();
-    debugScreen.print("setting to draw from bottom");
-  }
-
-  // change to red text
-  if( key === 'r') {
-    debugScreen.setTextColor(color(255,0,0));
-  }
-
-  // back to green text
-  if( key === 'g') {
-    debugScreen.setTextColor(color(255,0,0));
-  }
-
-  // hide background rect
-  if( key === 'x') {
-    debugScreen.setDrawBackgroundRect(false);
-  }
-
-  // show background rect
-  if( key === 'y') {
-    debugScreen.setDrawBackgroundRect(true);
-    return;
-  }
-
-  // turn on autoscroll
-  if( key === 'a' ) {
-    debugScreen.setAutoScroll(true);
-    return;
-  }
-
-  // turn off autoscroll
-  if( key === 'm' ) {
-    debugScreen.setAutoScroll(false);
-    return;
-  }
+  // every animation needs a descriptor, since we aren't switching animations, this string value doesn't matter
+  chase_image.resize(158,122); 
+  chase_image_2.resize(158,122); 
+  playerSprite.addAnimation('right', chase_image, chase_image);
+  playerSprite.addAnimation('left', chase_image_2, chase_image_2);
   
-  // increase font size
-  if (keyCode === UP_ARROW) {
-    lineCount++;
-    debugScreen.setLineCount(lineCount);
-    debugScreen.setTextSize(debugScreen.getTextSize()+1);
+
+  // use this to track movement from toom to room in adventureManager.draw()
+  adventureManager.setPlayerSprite(playerSprite);
+
+  // this is optional but will manage turning visibility of buttons on/off
+  // based on the state name in the clickableLayout
+  adventureManager.setClickableManager(clickablesManager);
+
+    // This will load the images, go through state and interation tables, etc
+  adventureManager.setup();
+
+  // call OUR function to setup additional information about the p5.clickables
+  // that are not in the array 
+  setupClickables(); 
+}
+
+// Adventure manager handles it all!
+function draw() {
+  // draws background rooms and handles movement from one to another
+  adventureManager.draw();
+
+  // draw the p5.clickables, in front of the mazes but behind the sprites 
+  clickablesManager.draw();
+
+  // No avatar for Splash screen or Instructions screen
+  if( adventureManager.getStateName() !== "Start" && 
+      adventureManager.getStateName() !== "Instruction" ) {
+      
+    // responds to keydowns
+    moveSprite();
+
+    // this is a function of p5.js, not of this sketch
+    drawSprite(playerSprite);
+  } 
+}
+
+// pass to adventure manager, this do the draw / undraw events
+function keyPressed() {
+  // toggle fullscreen mode
+  if( key === 'f') {
+    fs = fullscreen();
+    fullscreen(!fs);
+    return;
   }
 
-  // decrease font size
-  if (keyCode === DOWN_ARROW) {
-    lineCount--;
-    debugScreen.setLineCount(lineCount);
-    debugScreen.setTextSize(debugScreen.getTextSize()-1);
-  }
+  // dispatch key events for adventure manager to move from state to 
+  // state or do special actions - this can be disabled for NPC conversations
+  // or text entry   
 
-  // if wr are on autoscroll, output to specific line
-  if( debugScreen.getAutoScroll() === false ) {
-    // output to line #2
-    debugScreen.print("keyPressed | keyPressed = " + key, 1 );
+  // dispatch to elsewhere
+  adventureManager.keyPressed(key); 
+}
+
+function mouseReleased() {
+  adventureManager.mouseReleased();
+}
+
+//-------------- YOUR SPRITE MOVEMENT CODE HERE  ---------------//
+function moveSprite() {
+  if(keyIsDown(RIGHT_ARROW)) {
+    playerSprite.velocity.x = 10;
+    playerSprite.changeAnimation('right');
   }
-  else {
-    // show keycode on the screen
-    debugScreen.print("keyPressed | keyPressed = " + key );
+  else if(keyIsDown(LEFT_ARROW)){
+    playerSprite.changeAnimation('left'); 
+    playerSprite.velocity.x = -10;
+  }
+  else
+    playerSprite.velocity.x = 0;
+
+  if(keyIsDown(DOWN_ARROW))
+    playerSprite.velocity.y = 10;
+  else if(keyIsDown(UP_ARROW))
+    playerSprite.velocity.y = -10;
+  else
+    playerSprite.velocity.y = 0;
+}
+
+//-------------- CLICKABLE CODE  ---------------//
+
+function setupClickables() {
+  // All clickables to have same effects
+  for( let i = 0; i < clickables.length; i++ ) {
+    clickables[i].onHover = clickableButtonHover;
+    clickables[i].onOutside = clickableButtonOnOutside;
+    clickables[i].onPress = clickableButtonPressed;
   }
 }
 
-function mousePressed() {
-  // Form a long string with this mouse clicked information
-  debugScreen.print("mouse clicked, x = " + mouseX + " | y = " + mouseY + " | millis() = " + Math.round(millis()) );
+// tint when mouse is over
+clickableButtonHover = function () {
+  this.color = "#AA33AA";
+  this.noTint = false;
+  this.tint = "#FF0000";
+}
+
+// color a light gray if off
+clickableButtonOnOutside = function () {
+  // backto our gray color
+  this.color = "#AAAAAA";
+}
+
+clickableButtonPressed = function() {
+  // these clickables are ones that change your state
+  // so they route to the adventure manager to do this
+  adventureManager.clickablePressed(this.name); 
+}
+
+
+
+//-------------- SUBCLASSES / YOUR DRAW CODE CAN GO HERE ---------------//
+
+
+// Instructions screen has a backgrounnd image, loaded from the adventureStates table
+// It is sublcassed from PNGRoom, which means all the loading, unloading and drawing of that
+// class can be used. We call super() to call the super class's function as needed
+class InstructionsScreen extends PNGRoom {
+  // preload is where we define OUR variables
+  // Best not to use constructor() functions for sublcasses of PNGRoom
+  // AdventureManager calls preload() one time, during startup
+  preload() {
+    // These are out variables in the InstructionsScreen class
+    this.textBoxWidth = (width/6)*4;
+    this.textBoxHeight = (height/6)*4; 
+
+    // hard-coded, but this could be loaded from a file if we wanted to be more elegant
+    this.instructionsText = "You are navigating through the interior space of your moods. There is no goal to this game, but just a chance to explore various things that might be going on in your head. Use the ARROW keys to navigate your avatar around.";
+  }
+
+  // call the PNGRoom superclass's draw function to draw the background image
+  // and draw our instructions on top of this
+  draw() {
+    // tint down background image so text is more readable
+    tint(128);
+      
+    // this calls PNGRoom.draw()
+    super.draw();
+      
+    // text draw settings
+    fill(255);
+    textAlign(CENTER);
+    textSize(30);
+
+    // Draw text in a box
+    text(this.instructionsText, width/6, height/6, this.textBoxWidth, this.textBoxHeight );
+  }
 }
 
